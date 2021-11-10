508aef470789ec79aafa2a042ff16a7f2784d6bb
Adi-Davidovich <adidaflower@gmail.com>
2021-11-10 10:43:15 +0200

    clean

--- js/pages/keep-app.cmp.js
+++ js/pages/keep-app.cmp.js
@@ -2,11 +2,6 @@ export default {
     template:`
         <section class="home-page app-main">
             <h3>KEEP APP...</h3>
-            <h3>KEEP APP...</h3>
-            <h3>Eitans push APP...</h3>
-            <h3>Eitans 2nd push APP...</h3>
-
-            <h3>adi...</h3>
         </section>
     `
 }
