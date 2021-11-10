fc182b0dc27b94e7ffbb187f690d2bf773d1d689
Eitan Pano <eitanpano95@gmail.com>
2021-11-10 13:17:21 +0200

    added routes and css cmps

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 // import bookApp from './pages/book-app.cmp.js';
 import homePage from './pages/home-page.cmp.js';
-import keepApp from './pages/keep-app.cmp.js';
 import emailApp from './pages/email-app.cmp.js';
+import keepApp from './pages/keep-app.cmp.js';
 // import bookDetails from './pages/book-details.cmp.js';
 // import aboutPage from './pages/about-page.cmp.js';
 // import aboutTeam from './pages/about-team.cmp.js';
@@ -18,12 +18,12 @@ const routes = [
         component: homePage
     },
     {
-        path: '/keep',
-        component: keepApp
-    },
-    {
         path: '/email',
         component: emailApp
+    },
+    {
+        path: '/keep',
+        component: keepApp
     },
     // {
     //     path: '/about',
