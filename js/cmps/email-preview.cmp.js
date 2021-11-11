3a5c71d446c17b576ba22dee67112cb9ee032909
Eitan Pano <eitanpano95@gmail.com>
2021-11-11 21:00:11 +0200

    added half starring system and styles

--- js/cmps/email-preview.cmp.js
+++ js/cmps/email-preview.cmp.js
@@ -3,9 +3,11 @@ export default {
     template: `
         <li :class="showRead" @mouseover="hover = true" @mouseleave="hover = false" >
             <router-link :to="'/email/'+email.id">
-                <p>{{ email.from }}</p>
-                <p>{{ email.subject }}</p>
-                <p>{{ email.body }}</p>
+                <button @click.prevent="toggleStar"><i :class="showStarred" aria-hidden="true"></i></button>
+                <!-- <i @click.prevent="toggleStar" class="fa fa-star" aria-hidden="true"></i> -->
+                <p class="from">{{ email.from }}</p>
+                <p class="subject">{{ email.subject }}</p>
+                <p class="body">{{ email.body }}</p>
                 <div v-if="hover">
                     <button @click.prevent="toggleRead">{{ showRead }}</button>
                     <button @click.prevent="$emit('removed', email.id)">Delete</button> 
@@ -17,22 +19,30 @@ export default {
     data() {
         return {
             isRead: null,
-            hover: false
-        }
+            isStarred: null,
+            hover: false,
+        };
     },
     methods: {
         toggleRead() {
             this.isRead = !this.isRead;
-            this.$emit('toggleRead', this.email.id, this.isRead)
-
-        }
+            this.$emit('toggleRead', this.email.id, this.isRead);
+        },
+        toggleStar() {
+            this.isStarred = !this.isStarred;
+            console.log('starred', this.email.id, this.isStarred);
+            this.$emit('starred', this.email.id, this.isStarred);
+        },
     },
     computed: {
+        showStarred() {
+            return (this.isStarred) ? 'fa fa-star txt-gold': 'far fa-star'; 
+        },
         showRead() {
             return (this.isRead) ? 'read' : 'Unread';
         },
         showSentAt() {
-            return new Date(this.email.sentAt).toLocaleDateString()
-        }
-    }
+            return new Date(this.email.sentAt).toLocaleDateString();
+        },
+    },
 };
